76a88e90dee157e9b4c6b02872a039a043e1c46e
statkkh <140682514+statkkh@users.noreply.github.com>
2023-10-14 03:58:43 +0000

    websocket chat 구현

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // eslint-disable-next-line prettier/prettier
   const app = await NestFactory.create(AppModule, {cors : true});
   await app.listen(4010);
 }
